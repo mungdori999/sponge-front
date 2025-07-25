d3d3799dd56f1a9994445f48f045df96b797d148
mungdori <mungdori999@gmail.com>
2025-07-25 11:27:31 +0900

    로그인 페이지 완성

--- src/component/Login/LoginBox.jsx
+++ src/component/Login/LoginBox.jsx
@@ -1,6 +1,18 @@
 import "../../css/Login/LoginBox.css";
-const LoginBox = () => {
-  return <div className=""></div>;
+
+const LoginBox = ({ title, text1, text2, img }) => {
+  return (
+    <div className="LoginBox">
+      <div className="login-text">
+        <div className="login-title">{title}</div>
+        <div className="login-description">
+          <div className="login-text1">{text1}</div>
+          <div className="login-text2">{text2}</div>
+        </div>
+      </div>
+      <img className="login-img" src={img} />
+    </div>
+  );
 };
 
 export default LoginBox;
--- src/page/LoginPage.jsx
+++ src/page/LoginPage.jsx
@@ -1,13 +1,30 @@
 import BackButton from "../component/common/BackButton";
 import Header from "../component/Header/Header";
-
+import "../css/Login/LoginPage.css";
 import LoginBanner from "../component/Login/LoginBanner";
+import LoginBox from "../component/Login/LoginBox";
+import loginImg1 from "../assets/login1.png";
+import loginImg2 from "../assets/login2.png";
 
 const LoginPage = () => {
   return (
     <div className="LoginPage">
       <Header leftchild={<BackButton />} />
       <LoginBanner />
+      <div className="LoginBox-wrapper">
+        <LoginBox
+          title={"견주로 로그인하기"}
+          text1={"문제행동 작성하고 "}
+          text2={"훈련사에게 상담받기"}
+          img={loginImg1}
+        />
+        <LoginBox
+          title={"훈련사로 로그인하기"}
+          text1={"문제행동 진단하고 "}
+          text2={"더 많은 사람에게 홍보하기"}
+          img={loginImg2}
+        />
+      </div>
     </div>
   );
 };
