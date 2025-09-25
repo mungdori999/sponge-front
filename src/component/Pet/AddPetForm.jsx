2fcf4a04b21001d4c84568d7eacd43c5a83dfbab
mungdori <mungdori999@gmail.com>
2025-09-25 11:09:25 +0900

    addpet form

--- src/component/Pet/AddPetForm.jsx
+++ src/component/Pet/AddPetForm.jsx
@@ -1,7 +1,26 @@
 import "../../css/Pet/AddPetForm.css";
 
 const AddPetForm = () => {
-  return <div className="AddPetForm"></div>;
+  return (
+    <div className="AddPetForm">
+      <div className="name">
+        <label>
+          반려견 이름<span className="required">*</span>
+        </label>
+        <input type="text" placeholder="예)멍멍이" />
+      </div>
+      <div className="age"></div>
+      <div className="weight"></div>
+      <div className="breed">
+        <label>
+          견종
+          <span className="required">*</span>
+        </label>
+        <input type="text" placeholder="예)웰시코기" />
+      </div>
+      <div className="gender"></div>
+    </div>
+  );
 };
 
 export default AddPetForm;
